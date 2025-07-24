b32eafdd396350b74b20827d71c2a6a279fc3fd3
daviferreirabraz <daviferreirabraz18@gmail.com>
2025-07-24 17:45:43 -0300

    Add files via upload

---
+++ character_body_3d.gd
@@ -0,0 +1,33 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+const sensibilidade = 0.3
+func _input(event):
+	if event is InputEventScreenDrag:
+		%cabeca.rotate_x(-deg_to_rad(event.relative.y) * sensibilidade)
+		rotate_y(-deg_to_rad(event.relative.x) * sensibilidade)
+		
+		%cabeca.rotation.x = clamp(%cabeca.rotation.x, deg_to_rad(-45), deg_to_rad(80))
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
